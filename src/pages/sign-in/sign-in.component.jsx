2b114867accc194450adb5263cdfc6d34f175620
yourwpmadesimple <wayne@yourwpmadesimple.com>
2021-11-19 13:46:58 -0700

    added button component for signin page and styling

--- src/pages/sign-in/sign-in.component.jsx
+++ src/pages/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 
 import FormInput from "../../components/form-input/form-input.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -49,7 +50,7 @@ class SignIn extends React.Component {
             required
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit"> Sign In </CustomButton>
         </form>
       </div>
     );
