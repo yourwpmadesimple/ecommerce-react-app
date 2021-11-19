85ceaf4f96121fb2544af0a7386514a1471f99be
yourwpmadesimple <wayne@yourwpmadesimple.com>
2021-11-19 10:32:03 -0700

    updated our app with shop page, collection preview component and& collection item component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => (
   <div>
@@ -16,6 +17,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={HomePage}></Route>
       <Route exact path="/hats" component={HatsPage}></Route>
+      <Route exact path="/shop" component={ShopPage}></Route>
     </Switch>
   );
 }
