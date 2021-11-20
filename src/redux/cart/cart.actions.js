0041ea85004b66b6bbfdf36b296eca3265798a05
yourwpmadesimple <wayne@yourwpmadesimple.com>
2021-11-20 09:59:37 -0700

    updated data, updated cart utis to group items in reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import { CartActionTypes } from "./cart.types";
 export const toggleCartHidden = (user) => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEM,
 });
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEMS,
+  payload: item,
+});
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,3 +1,4 @@
 export const CartActionTypes = {
   TOGGLE_CART_HIDDEM: "TOGGLE_CART_HIDDEM",
+  ADD_ITEMS: "ADD_ITEMS",
 };
