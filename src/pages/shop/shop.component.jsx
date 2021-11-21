0e8e116e968356005140238f3791ee729208f01c
yourwpmadesimple <wayne@yourwpmadesimple.com>
2021-11-21 14:22:40 -0700

    add collection componenet to pages

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,11 +3,17 @@ import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
+import CollectionPage from '../collection/collection.component';
+
 const ShopPage = ({ match }) => {
-  console.log(match);
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
     </div>
   );
 };
